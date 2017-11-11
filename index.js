4ada9ae3ab293e1d4d9ae29d7652c421cb293fcc
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-11 02:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ function overwriteBestCustomer(){
   bestCustomer = 'maybe bob';
 }
 const leastFavoriteCustomer = 'jessica';
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'bob';
+}
